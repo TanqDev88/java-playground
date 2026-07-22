c417e7e1f38eac5a3b0e200c51ee6e34e006e0e5
TanqDev88 <oliverprim783@gmail.com>
2026-07-21 23:49:29 -0300

    feat: add Exercise001CountOneToTen.java

---
+++ src/main/java/com/jorge/fundamentals/Exercise001CountOneToTen.java
@@ -0,0 +1,18 @@
+package com.jorge.fundamentals;
+
+public class Exercise001CountOneToTen {
+    /*
+    Ejercicio 1 - Contar del 1 al 10
+
+        Mostrar todos los números del 1 al 10 usando for.
+
+    Conceptos: bucle for + contador incremental.
+     */
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i);
+        }
+
+    }
+}
