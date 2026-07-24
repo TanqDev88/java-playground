0dedcca569195f03247b58b97c63810b76d97f99
TanqDev88 <oliverprim783@gmail.com>
2026-07-23 23:46:28 -0300

    feat: add Exercise002MultiplicationTable

---
+++ src/main/java/com/jorge/fundamentals/Exercise002MultiplicationTable.java
@@ -0,0 +1,16 @@
+package com.jorge.fundamentals;
+import java.util.Scanner;
+
+public class Exercise002MultiplicationTable {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int numero = sc.nextInt();
+
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(numero + " x " + i + " = " + (numero * i));
+        }
+
+        sc.close();
+    }
+}
