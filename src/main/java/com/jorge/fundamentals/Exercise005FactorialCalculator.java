89e7f394cbc75eb3db121552b9cff67136a35942
TanqDev88 <oliverprim783@gmail.com>
2026-07-30 12:58:13 -0300

    feat: add Exercise005FactorialCalculator

---
+++ src/main/java/com/jorge/fundamentals/Exercise005FactorialCalculator.java
@@ -0,0 +1,19 @@
+package com.jorge.fundamentals;
+import java.util.Scanner;
+
+public class Exercise005FactorialCalculator {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int numero = sc.nextInt();
+        long factorial = 1;
+
+        for (int i = 1; i <= numero; i++) {
+            factorial *= i;
+        }
+
+        System.out.println(factorial);
+
+        sc.close();
+    }
+}
