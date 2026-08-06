57e9fdceb73dcccb290342424801be37c01618bf
TanqDev88 <oliverprim783@gmail.com>
2026-08-06 20:48:24 -0300

    feat: add Exercise003NumberAccumulator

---
+++ src/main/java/com/jorge/fundamentals/Exercise003NumberAccumulator.java
@@ -0,0 +1,22 @@
+package com.jorge.fundamentals;
+import java.util.Scanner;
+
+public class Exercise003NumberAccumulator {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int numero;
+        int suma = 0;
+
+        numero = sc.nextInt();
+
+        while (numero != 0) {
+            suma += numero;
+            numero = sc.nextInt();
+        }
+
+        System.out.println(suma);
+
+        sc.close();
+    }
+}
